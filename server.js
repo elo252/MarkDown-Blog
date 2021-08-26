bbe71c1ce30de04c20f140ddb65408535de63de7
elo252 <drg020502@gmail.com>
2021-08-26 17:34:33 +0530

    Final Project

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
+const app = express()
+
+
+
+const validator = require('validator')
+
+const db = "mongodb+srv://user-ojasy:goaldiggers2020@cluster0.btfod.mongodb.net/Markdown?retryWrites=true&w=majority"
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+
+
+
+
+
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000)
